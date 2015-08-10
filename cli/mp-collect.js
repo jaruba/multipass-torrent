f5e183c3153224f49664a1cc383aac7866eeabac
Ivo Georgiev <ivo@linvo.org>
2015-08-10 17:24:09 +0300

    basic replication working on torrents DB

--- cli/mp-collect.js
+++ cli/mp-collect.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
 
 var argv = require("minimist")(process.argv.slice(2));
+var url = require("url");
+var net = require("net");
+var _ = require("lodash");
 
-module.dbPath = argv.dbPath || "./db";
+module.dbPath = argv["db-path"] || "./db";
 
 var db = require("../lib/db");
+db.listenReplications();
+if (argv.replicate) db.getSyncStream().pipe(net.connect(url.parse(argv.replicate).port));
+
+/*
+db.torrents.put("xxxxx", "test");
+db.torrents.put("testy test", "foo");
+db.torrents.put("another key", "is testing");*/
