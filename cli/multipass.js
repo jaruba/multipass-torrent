ed4f448d109551ce37a3c5aaf3777935803e6485
Ivo Georgiev <ivo@linvo.org>
2015-08-11 17:46:34 +0300

    Advancements in indexing logic

--- cli/multipass.js
+++ cli/multipass.js
@@ -42,7 +42,7 @@ importQueue.push({ url: "https://torrentz.eu/feed_verified?q=" });
 /* Process & index infoHashes
  */
 var processQueue = async.queue(function(task, next) {
-	indexer.index(task.infoHash, task.extra, next);
+	indexer.index(task, { }, next);
 }, 5);
 
 /* Log number of torrents we have
