ff500ca6099af0eccfdbe72722285fc2ce80ec05
sammuel86 <sammuel86@popcorntime.io>
2015-08-28 13:41:54 -0700

    json preliminaries
    add deps, populate json importer

--- importers/json.js
+++ importers/json.js
@@ -1,2 +1,11 @@
-/* TODO: JSON importing for YTS and EZTV APIs
- */
+var JSONStream = require('JSONStream');
+var es = require('event-stream');
+
+var log = require("../lib/log");
+
+// This should emit results up through an EventEmitter or a pipe, not use collect directly
+
+module.exports = function(stream, source)
+{
+    return stream = stream.pipe(process.stdout)
+}
