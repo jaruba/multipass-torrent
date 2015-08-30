bcc1d7aabc44fccd9c80280c34941a35745adb68
sammuel86 <sammuel86@popcorntime.io>
2015-08-30 10:45:27 -0700

    Magics.
    add some infos
    
    remove that
    
    add in host checks for fallbacks of eztv
    
    dynamic hosts
    
    remove underscore when we already have lodash

--- importers/json.js
+++ importers/json.js
@@ -4,68 +4,98 @@ var needle = require('needle');
 var Q = require('q');
 var url = require('url');
 var async = require('async');
-
+var _ = require('lodash');
 var log = require('../lib/log');
-
-var eztvEndpoints = ['eztvapi.re', 'tv.ytspt.re', 'api.popcorntime.io', 'api.popcorntime.cc'];
 
 // This should emit results up through an EventEmitter or a pipe, not use collect directly
 
 module.exports = function(stream, source) {
-    return Q.all([getjson(source.url), parsesource(source.url)]).spread(importjson);
+    return Q.all([getjson(source.url), parsesource(url.parse(addhttp(source.url)))]).spread(importjson);
 }
 
 function importjson(json, host) {
-    if (host === 'unknown') {
+    if (host.parsedName === 'unknown') {
         log.error('json unknown source', source);
         return;
     }
-    switch (host) {
+    switch (host.parsedName) {
         case 'eztv':
-            processEztv();
+            processEztv(host);
             break;
         case 'yts':
             break;
     }
 }
 
-function processEztv() {
+function processEztv(host) {
     var queue = async.queue(function(task, next) {
         getjson(task).then(function(response) {
-            console.log(task.toString());
             if (response.episodes) {
-                console.log(JSON.stringify(response))
-            } else if (response.imdb_id) {
-                console.log(JSON.stringify(response))
+                var returnObject = {
+                    show_name: response.title,
+                    show_imdb: response.imdb_id,
+                    verified: true
+                }
+                response.episodes.forEach(function(item) {
+                    var availableTorrents = {};
+                    _.forEach(_.omit(item.torrents, '0'), function(t, key) { // quality 0 === 420p we remove to avoid duplicates 
+                        availableTorrents[key] = t.url;
+                    });
+                    _.assign(returnObject, {
+                        episode_tvdb: item.tvdb_id,
+                        episode_name: item.title,
+                        episode_torrents: availableTorrents
+                    });
+                    console.log('Found:', returnObject.show_name, '-', 'S' + item.season + 'E' + item.episode, '-', item.title, '-', _.without(Object.keys(item.torrents), '0'))
+                });
+            } else if (response[0].imdb_id) {
                 response.forEach(function(item) {
-                    console.log(item)
-                    queue.push('https://eztvapi.re/show/' + item)
+                    if (host.slashes) {
+                        queue.push(host.protocol + '//' + host.host + '/show/' + item.imdb_id)
+                    } else {
+                        queue.push(host.protocol + host.host + '/show/' + item.imdb_id)
+                    }
                 });
             } else if (response && response.length > 0) {
                 response.forEach(function(url) {
-                    queue.push('https://eztvapi.re/' + url)
+                    if (host.slashes) {
+                        queue.push(host.protocol + '//' + host.host + '/' + url)
+                    } else {
+                        queue.push(host.protocol + host.host + '/' + url)
+                    }
                 });
             }
             process.nextTick(next);
         });
     }, 2);
-    queue.push('https://eztvapi.re/shows');
+    if (host.slashes) {
+        queue.push(host.protocol + '//' + host.host + '/shows')
+    } else {
+        queue.push(host.protocol + host.host + '/shows')
+    }
 }
 
 function parsesource(s) {
-    var host,
-        s = url.parse(s);
-    switch (s.host) {
-        case 'eztvapi.re':
-            host = 'eztv';
-            break;
-        case 'yts.to':
-            host = 'yts';
-            break;
-        default:
-            host = 'unknown';
+
+    var eztvEndpoints = ['eztvapi.re', 'tv.ytspt.re', 'api.popcorntime.io', 'api.popcorntime.cc', 'http://7aa7xwqtxoft27r2.onion'];
+    var ytsEndpoints = ['yts.to', 'yts.io', 'yts.sh', 'http://gm6gttbnl3bjulil.onion'];
+
+    if (new RegExp(eztvEndpoints.join('|')).test(s.host)) {
+        s.parsedName = 'eztv';
+    } else if (new RegExp(ytsEndpoints.join('|')).test(s.host)) {
+        s.parsedName = 'yts';
+    } else {
+        s.parsedName = 'unknown';
     }
-    return Q(host);
+
+    return Q(s);
+}
+
+function addhttp(url) {
+    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+        url = "http://" + url;
+    }
+    return url;
 }
 
 function getjson(url) {
