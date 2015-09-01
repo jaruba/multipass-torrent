8cf6ab678a22c6d6c96ff2646325b60e2235a2ba
jaruba <branza.alex@gmail.com>
2015-09-02 00:05:23 +0300

    Added Buffering Event

--- cli/multipass.js
+++ cli/multipass.js
@@ -12,6 +12,10 @@ var db = require("../lib/db");
 var indexer = require("../lib/indexer");
 var importer = require("../lib/importer");
 
+var events_mod = require('events');
+var events = new events_mod.EventEmitter();
+var buffer = [];
+
 db.listenReplications(cfg.dbId); // start our replication server
 db.findReplications(cfg.dbId); // replicate to other instances
 
@@ -19,10 +23,12 @@ db.findReplications(cfg.dbId); // replicate to other instances
  */
 var importQueue = async.queue(function(source, next) {
 	source = typeof(source) == "string" ? { url: source } : source;
+	buffer[source.url] = { progress: 0, total: 0 };
 	log.important("importing from "+source.url);
 	importer.collect(source, function(err, status) {
 		if (err) log.error(err);
 		else log.important("importing finished from "+source.url+", "+status.found+" infoHashes, "+status.imported+" of them new, through "+status.type+" importer ("+(status.end-status.start)+"ms)");
+		buffer[source.url].total = parseInt(status.found);
 
 		if (source.interval) setTimeout(function() { importQueue.push(source) }, source.interval); // repeat at interval - re-push
 
@@ -44,7 +50,11 @@ var processQueue = async.queue(function(task, _next) {
 
 	// consider using db.indexes.seeders to figure out a skip case here; don't overcomplicate though
 	db.get(task.infoHash, function(err, res) {
-		if (err) { log.error(err); return next(); }
+		if (err) {
+			buffering(task.source.url);
+			log.error(err);
+			return next();
+		}
 		
 		// Pass a merge of existing torrent objects as a base for indexing		
 		task.torrent = res && res.length && indexer.merge(res.sort(function(a, b) { return a.seq - b.seq }).map(function(x) { return x.value }));
@@ -53,7 +63,10 @@ var processQueue = async.queue(function(task, _next) {
 			function(cb) { indexer.index(task, { }, cb) },
 			function(cb) { (task.torrent && task.torrent.popularityUpdated > (Date.now() - 6*60*60*1000)) ? cb() : indexer.seedleech(task.infoHash, cb) }
 		], function(err, indexing) {
-			if (err) { if (task.callback) task.callback(err); log.error(task.infoHash, err); return next(); }
+			if (err) {
+				buffering(task.source.url);
+				if (task.callback) task.callback(err); log.error(task.infoHash, err); return next();
+			}
 
 			var torrent = _.merge(indexing[0], indexing[1] ? { popularity: indexing[1], popularityUpdated: Date.now() } : { });
 			db.merge(torrent.infoHash, res, torrent); // TODO think of cases when to omit that
@@ -63,15 +76,27 @@ var processQueue = async.queue(function(task, _next) {
 
 			if (torrent.uninteresting && !res.length) log.warning(torrent.infoHash+" / "+torrent.name+" is non-interesting, no files indexed");
 			log.hash(task.infoHash, "processed");
+			buffering(task.source.url);
 		});
 	});
 }, 6);
+
+/* Emit buffering event
+ */
+function buffering(source) {
+	buffer[source].progress++;
+	perc = buffer[source].progress/buffer[source].total;
+	perc = (Math.floor(perc * 100) / 100).toFixed(2);
+	events.emit('Buffering', source, perc);
+	if (perc == 1) delete buffer[source];
+}
 
 /* Programatic usage of this
  */
 if (module.parent) return module.exports = {
 	processQueue: processQueue,
 	importQueue: importQueue,
+	onBuffering: function(cb){ events.on('Buffering',cb); return this }
 };
 
 /* Log number of torrents we have
