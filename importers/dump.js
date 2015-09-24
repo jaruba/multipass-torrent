8e685887288a2ff84e6f4d101cd787c04c77b058
Ivo Georgiev <ivo@linvo.org>
2015-09-24 09:42:54 +0300

    Send hints url to cinemeta when we are crawling from KAT - because it contains the IMDB ID on the page

--- importers/dump.js
+++ importers/dump.js
@@ -1,5 +1,5 @@
 var byline = require("byline");
-
+var _ = require("lodash");
 var log = require("../lib/log");
 
 module.exports = function(stream, source)
@@ -8,13 +8,17 @@ module.exports = function(stream, source)
     .on("data", function(line)
     {
         var parts = line.toString().split("|"),
-            infoHash = parts[0], cat = (parts[2] || "").toLowerCase();
+            infoHash = parts[0], cat = (parts[2] || "").toLowerCase(),
+            additional = { }; // additional info found
 
+        var infoUrl = parts[3];
+        if (infoUrl.match("kat.cr")) additional.hints = { url: infoUrl }; // OR any torrent website that we know contains IMDB ID on it's info page
+        
         if (cat.match("porn") || cat.match("adult")) return; // exclude that for now
 
         //if (cat.match("video") && (cat.match("dvd") || cat.match("rip") || cat.match("tv")))
         if (cat.match("movies") || cat.match("dvd") || cat.match("rip") || cat.match("tv")/* || cat.match("video")*/) {
-            stream.emit("infoHash", infoHash, source.addon);
+            stream.emit("infoHash", infoHash, _.extend(additional, source.addon || { }));
         };
     })
     .on("error", function(err) { log.error("dump", err) });
