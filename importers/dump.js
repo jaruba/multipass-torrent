c3f4a64224f14d9e2a19736bd65f1cf174f48b14
Ivo Georgiev <ivo@linvo.org>
2015-10-20 21:50:24 +0300

    Dump: match IMDB ID from dumps, e.g. yts

--- importers/dump.js
+++ importers/dump.js
@@ -18,7 +18,12 @@ module.exports = function(stream, source)
 
         var infoUrl = parts[3];
         if (infoUrl.match("kat.cr|kickass.to")) additional.hints = { url: infoUrl }; // OR any torrent website that we know contains IMDB ID on it's info page
-        
+            
+        if (parts[4].match(".torrent$")) additional.download = parts[4]; // URL to torrent file
+
+        var imdbMatch = (parts[5] && parts[5].match("(tt[0-9]+)")) || (parts[3] && parts[3].match("(tt[0-9]+)"));
+        if (imdbMatch) additional.hints = { imdb_id: imdbMatch && imdbMatch[0] }; // no issue to override, because the hint url is for getting imdb_id anyway
+
         if (cat.match("porn") || cat.match("adult")) return; // exclude that for now
 
         //if (cat.match("video") && (cat.match("dvd") || cat.match("rip") || cat.match("tv")))
