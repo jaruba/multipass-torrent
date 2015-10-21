08ef990baeb4b8662e012af553fb696d8f126c3e
Ivo Georgiev <ivo@linvo.org>
2015-10-21 17:26:04 +0300

    fix eztv error handling

--- importers/json.js
+++ importers/json.js
@@ -69,6 +69,9 @@ module.exports = function(stream, source) {
                 }
 
                 process.nextTick(next);
+            }, function(err) {
+                log.error(err);
+                process.nextTick(next);
             });
         }, 2);
         
