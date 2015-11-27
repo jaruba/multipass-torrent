39f04bad853221963f0e6e065839f4f5c216716e
Ivo Georgiev <ivo@linvo.org>
2015-11-27 20:06:07 +0200

    Generic importer: avoid matching parts of magnet link

--- importers/generic.js
+++ importers/generic.js
@@ -5,7 +5,9 @@ module.exports = function(stream, source)
     return stream = stream.pipe(byline.createStream()).on("data", function(line)
     {
         /* Cut the string into RegEx. this is my last resort. */
-        var hashes = line.toString().match(new RegExp("([0-9A-Fa-f]){40}", "g"));
+        var line = line.toString(), match, hashes = [];
+        var regex = new RegExp("(?:%3A)(([0-9A-Fa-f]){40})", "g");
+        while (match = regex.exec(line)) hashes.push(match[1]);
         if (hashes) hashes.forEach(function(hash) { stream.emit("infoHash", hash, source.addon) });
     });
 };
